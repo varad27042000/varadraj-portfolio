026103bb30021596534c4031c9323f9ee72656e8
Vk27-keelworks <varadraj.kini@keelworks.org>
2025-10-01 20:35:56 -0400

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/varadraj-portfolio/",
+  base: process.env.VITE_BASE_PATH || "/varadraj-portfolio",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
